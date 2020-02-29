ec89ce0c1754d897c3d34070449084bf418774e1
petermeissner <retep.meissner@gmail.com>
2020-02-29 23:21:18 +0100

    passing checks

---
+++ R/kafkaesquejars-package.R
@@ -0,0 +1,11 @@
+#' Java Archive Wrapper Supporting the 'kafkaesque' Package
+#'
+#' Kafka is a big data technology providing a scalable, persistent, distributed,
+#' high throughput event log to which clients can subscribe topics or publish
+#' messages.
+#'
+#' @md
+#' @name kafkaesquejars
+#' @docType package
+#' @author Peter Meissner <retep.meissner@@gmail.com>
+NULL
--- R/zzz.r
+++ R/zzz.r
@@ -1,9 +1,3 @@
 .onLoad <-
   function(libname, pkgname) {
-    rJava::.jpackage(pkgname, jars = "*", lib.loc = libname)
-    rJava::.jaddClassPath(
-      list.files(
-        system.file("java", package = pkgname)
-      )
-    )
   }
